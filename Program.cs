7a8c7b394810748b19c46bb614c3adce6ef60884
Maycon Rodrigues <93656419+maycon37@users.noreply.github.com>
2025-05-20 00:47:35 -0300

    4.0

--- Program.cs
+++ Program.cs
@@ -5,3 +5,19 @@ Pessoa pessoal = new Pessoa();
 pessoal.Nome = "Maycon Rodrigues";
 pessoal.Idade = 33;
 pessoal.Apresentar();
+
+string apresentaçao = "Olá, seja bem vindo!!";
+int quantidade = 1;
+double altura = 1.89;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresentaçao);
+Console.WriteLine("Valor da variável quantidade: "+ quantidade);
+Console.WriteLine("Valor da variável altura: "+ altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preço: "+ preco);
+Console.WriteLine("Valor da variável condição: " + condicao);
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+Console.WriteLine("Hoje é dia: " + dataAtual.ToString("dd")+", do mês de "+dataAtual.ToString("MMMM")+", no ano de "+ dataAtual.ToString("yyyy"));
