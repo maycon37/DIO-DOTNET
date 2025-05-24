1eca6695f971d453a3215dd9953d6af1a12cbd5b
Maycon Rodrigues <93656419+maycon37@users.noreply.github.com>
2025-05-24 11:15:18 -0300

    5.0

--- Program.cs
+++ Program.cs
@@ -8,26 +8,27 @@ pessoal.Idade = 33;
 pessoal.Apresentar();
 
 string apresentaçao = "Olá, seja bem vindo!!";
+Console.WriteLine("Informações sobre o usuário:");
 int quantidade = 1;
 double altura = 1.89;
 decimal preco = 1.80M;
 bool condicao = true;
 
 Console.WriteLine(apresentaçao);
-Console.WriteLine("Valor da variável quantidade: "+ quantidade);
-Console.WriteLine("Valor da variável altura: "+ altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preço: "+ preco);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preço: " + preco);
 Console.WriteLine("Valor da variável condição: " + condicao);
 
 DateTime dataAtual = DateTime.Now;
 Console.WriteLine(dataAtual);
-Console.WriteLine("Hoje é dia: " + dataAtual.ToString("dd")+", do mês de "+dataAtual.ToString("MMMM")+", no ano de "+ dataAtual.ToString("yyyy"));
+Console.WriteLine("Hoje é dia: " + dataAtual.ToString("dd") + ", do mês de " + dataAtual.ToString("MMMM") + ", no ano de " + dataAtual.ToString("yyyy"));
 
 int quantidaEmEstoque = 10;
 int quantidadedeCompra = 4;
 bool possivelVenda = quantidadedeCompra > 0 && quantidaEmEstoque >= quantidadedeCompra;
-if (possivelVenda)
 
+Console.WriteLine("Condicionais:");
 Console.WriteLine($"Quantidade em estoque: {quantidaEmEstoque}");
 Console.WriteLine($"Quantidade compra :{quantidadedeCompra}");
 Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
@@ -43,4 +44,23 @@ else if (possivelVenda)
 else
 {
     Console.WriteLine("Venda negada! não temos a quantidade deseja no estoque.");
+}
+
+Console.WriteLine("Digite uma letra:");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal!");
+        break;
+
+    default:
+        Console.WriteLine("Não é uma vogal.");
+        break;
+
 }
